ea0e18e2e07ca6429029d3b90d917a0d0dd963b3
Abk47 <abknshunju@gmail.com>
2026-03-24 08:53:39 +0300

    Remove gallery carousel arrows and related code

--- src/js/main.js
+++ src/js/main.js
@@ -57,8 +57,6 @@ document.addEventListener('DOMContentLoaded', function() {
   // Gallery Carousel
   const gslides = document.querySelectorAll('.gslide');
   const gdotsEl = document.getElementById('gdots');
-  const gleft = document.querySelector('.garrow-left');
-  const gright = document.querySelector('.garrow-right');
   let gcur = 0;
   gslides.forEach((_, i) => {
     const d = document.createElement('button');
@@ -75,11 +73,7 @@ document.addEventListener('DOMContentLoaded', function() {
   }
   setInterval(() => gGoTo(gcur + 1), 4000);
 
-  // Manual navigation: arrow buttons
-  if (gleft && gright) {
-    gleft.addEventListener('click', () => gGoTo(gcur - 1));
-    gright.addEventListener('click', () => gGoTo(gcur + 1));
-  }
+  // Manual navigation: arrow buttons removed
 
   // Touch swipe for gallery
   const gcarousel = document.querySelector('.gcarousel');
